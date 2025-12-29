1399c209b1df59e45db398872f6874f5c6343fba
jb1567us <baron.joshua@gmail.com>
2025-12-29 12:14:46 -0600

    chore: cleanup repository and package b2b tool

--- product_insights - Copy/_legacy_php_backup/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: /product_insights/app/views/input_product.php");
-exit;
-?>
--- product_insights - Copy/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Simple configuration
-// In production, you might want to load this from a real system environment variable or a file outside public_html
-
-return [
-    'gemini_api_key' => getenv('GEMINI_API_KEY') ?: 'AIzaSyBbjG2u56kVoh7fFKZ-nf5yEAsIOJh_UbI',
-];
