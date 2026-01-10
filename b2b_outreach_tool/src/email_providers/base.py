6e340333aad2a830958aa0b0037de099cf3a8e19
jb1567us <baron.joshua@gmail.com>
2026-01-10 09:02:58 -0600

    Cleanup: Remove extraneous root files, keeping only b2b_outreach_tool

--- b2b_outreach_tool/src/email_providers/base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class EmailProvider(ABC):
-    @abstractmethod
-    def send_html_email(self, to_email, subject, html_content):
-        """Sends an HTML email. Returns True on success, False on failure."""
-        pass
--- refactor_tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .golden_master import GoldenMaster
-from .legacy_keeper import LegacyKeeper
-from .preservation import PreservationChecklist
